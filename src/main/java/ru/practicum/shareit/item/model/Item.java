4e8c55ae919d6fc08f53dee495dd534a18a0a1be
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2025-02-27 19:16:22 +0500

    ФЗ 15 - user JPA

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,14 +9,27 @@ import lombok.Setter;
 /**
  * TODO Sprint add-controllers.
  */
+@Entity
+@Table(name = "item")
 @Builder
 @Getter
 @Setter
 @EqualsAndHashCode
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
+
+    @Column(name = "available")
     private boolean available;
+
+    @Column(name = "owner_id")
     private Long ownerId;
+
+    public Item() {
+    }
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,12 +9,22 @@ import lombok.Setter;
 /**
  * TODO Sprint add-controllers.
  */
+@Entity
+@Table(name = "users")
 @Builder
 @Getter
 @Setter
 @EqualsAndHashCode
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String name;
+
+    public User() {
+    }
 }
